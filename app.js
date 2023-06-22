627587235513844bd83babb6faa5d90a21973180
HotG8119 <k508060@gmail.com>
2023-06-22 21:49:40 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const routes = require("./routes");
+
+const usePassport = require("./config/passport");
 require("./config/mongoose");
 
 const app = express();
@@ -28,6 +30,7 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+usePassport(app);
 app.use(routes);
 
 app.listen(PORT, () => {
